d071519abe855879510c4555ae844b257c705b52
Nadia Santiago <nadusantiago@gmail.com>
2022-10-23 16:49:18 -0300

    agregar productos al carrito

--- script/main.js
+++ script/main.js
@@ -1,37 +1,148 @@
+/*--------------DECLARACION DE PRODUCTOS---------------*/
 class Productos {
-    constructor (nombre, precio, stock,cantidad){
+    constructor (id, nombre, precio, img, stock, cantidad){
+        this.id=id;
         this.nombre=nombre;
         this.precio=precio;
+        this.img=img;
         this.stock=stock;
         this.cantidad=cantidad;
     }
 }
+
 /*--------------DECLARACION DE PRODUCTOS---------------*/
-const producto1 = new Productos ("aceite",420,100,0);
-const producto2 = new Productos ("yerba",360,50,0);
-const producto3 = new Productos ("arveja",53,30,0);
-const producto4 = new Productos ("durazno",350,60,0);
-const producto5 = new Productos ("gaseosa",390,40,0);
-const producto6 = new Productos ("cerveza",370,80,0);
+const aceite = new Productos ("item-1","Aceite",420,"./img/aceiteCocinero.jpg",100,0);
+const yerba = new Productos ("item-2","Yerba",360,"./img/yerbaPlayadito.jpg",50,0);
+const arveja = new Productos ("item-3","Arveja",53,"./img/arvejaVigente.jpg",30,0);
+const durazno = new Productos ("item-4","Durazno",350,"./img/duraznoVigente.jpg",60,0);
+const gaseosa = new Productos ("item-5","Gaseosa",390,"./img/gaseosaCoca.jpg",40,0);
+const cerveza = new Productos ("item-6","Cerveza",370,"./img/brahma.jpg",80,0);
 
-listaProductos = [producto1,producto2,producto3,producto4,producto5,producto6];
+const contenedorProductos = document.querySelector(".contenedor");
+
+let listaProductos = [aceite,yerba,arveja,durazno,gaseosa,cerveza];
+let carrito = [];
+
+listaProductos.forEach(producto =>{
+    contenedorProductos.innerHTML +=`
+        <div class="contenedor-productos" id="${producto.id}">
+            <img src="${producto.img}" class="product-img" alt="${producto.nombre}">
+            <div class="contenedor-productos-descripcion">
+                <h2>${producto.nombre}</h2>
+                <span class="product-price">$${producto.precio}</span>
+            </div>
+            <button class="add-to-cart">Comprar</button>
+        </div>
+    `
+})
+
+const botonesComprar = document.querySelectorAll(".add-to-cart");
+const btnCarrito = document.querySelector("#cart");
+const ventanaCarrito = document.querySelector(".cart-modal-overlay");
+const cerrarCarrito = document.querySelector("#close-btn");
+const contenedorCarrito = document.querySelector(".product-rows");
+const totalCarrito = document.querySelector(".total-price");
+const totalProducto = document.querySelector(".cart-quantity");
+
+btnCarrito.addEventListener("click",()=>{
+    ventanaCarrito.classList.add("open");
+});
+
+cerrarCarrito.addEventListener("click",()=>{
+    ventanaCarrito.classList.remove("open");
+});
+
+botonesComprar.forEach(boton => {
+    boton.addEventListener("click", agregarCarrito);
+});
+
+
+
+
+function agregarCarrito(e){
+    boton = e.target;
+    let contenedorPadre = boton.parentElement;
+    let prodID = contenedorPadre.getAttribute("id");
+    const prodCarrito = listaProductos.find(elemento => elemento.id == prodID);
+    carrito.push(prodCarrito);
+    popularCarrito();
+    actualizarCantidadCarrito();
+}
+
+function popularCarrito(){
+    contenedorCarrito.innerHTML = '';
+    carrito.forEach(producto => {
+        contenedorCarrito.innerHTML += `
+            <div class='product-row' id='${producto.id}'>
+                <img src='${producto.img}' class='cart-image' />
+                <span class='cart-product'>${producto.nombre}</span>
+                <span class='cart-price'>$${producto.precio}</span>
+                <input type='number' value='1' class="product-quantity" />
+                <button id="btn-remove"><i class="bi bi-trash3"></i></button>
+            </div>
+        `
+    });
+    actualizarTotal();
+}
+
+function actualizarTotal(){
+    let total = carrito.reduce((acc, producto)=>{ 
+        return acc + producto.precio 
+    },0)
+    totalCarrito.innerHTML = `$${total}`
+}
+
+function actualizarCantidadCarrito (){
+    totalProducto.textContent = carrito.length;
+}
+
+const removeElement = document.querySelectorAll("#btn-remove");
+removeElement.forEach(boton => {
+    boton.addEventListener("click", quitarElemento);
+});
+
+function quitarElemento(e){
+    boton = e.target;
+    console.log("hola")
+    // let contenedorPadre = boton.parentElement;
+    // let prodID = contenedorPadre.getAttribute("id");
+    // let indice = carrito.indexOf(prodID);
+    // carrito.splice(indice,1);
+    // popularCarrito();
+    // actualizarCantidadCarrito();
+}
+//para borrar un producto usar splice
+
+
+
+
+
+/*
 carrito =[];
-let totalCarrito = 0;
-
+let producto = 0;
+let totalCarrito =0;
 function compraProductos(){
-    let producto = prompt(`¿qué producto desea comprar? ${producto1.nombre}($${producto1.precio}), ${producto2.nombre}($${producto2.precio}), ${producto3.nombre}($${producto3.precio}), ${producto4.nombre}($${producto4.precio}), ${producto5.nombre}($${producto5.precio}), ${producto6.nombre}($${producto6.precio})`).toLowerCase();
+    producto = prompt(`¿qué producto desea comprar? ${aceite.nombre}($${aceite.precio}), ${yerba.nombre}($${yerba.precio}), ${arveja.nombre}($${arveja.precio}), ${durazno.nombre}($${durazno.precio}), ${gaseosa.nombre}($${gaseosa.precio}), ${cerveza.nombre}($${cerveza.precio})`).toLowerCase();
     const isProduct = listaProductos.find(elemento => elemento.nombre == producto);
-    
     if (isProduct){
         let cantidad = (parseInt(prompt("¿que cantidad desea comprar?(ingresar en numero)")));
         while(isNaN(cantidad) == true){
             alert(`El dato ingresado es invalido, debe ingresar un numero (por ejemplo: 1,2,3)`);
             cantidad = (parseInt(prompt("¿que cantidad desea comprar?(ingresar en numero)")));
         }
+        while(isProduct.stock < cantidad){
+            cantidad = (parseInt(prompt (`No hay suficiente stock! quedan ${isProduct.stock} unidades de ${producto}. ¿Qué cantidad desea comprar?`)))
+            while(isNaN(cantidad) == true){
+                alert(`El dato ingresado es invalido, debe ingresar un numero (por ejemplo: 1,2,3)`);
+                cantidad = (parseInt(prompt("¿que cantidad desea comprar?(ingresar en numero)")));
+            }
+        }
         if (carrito.find(elemento=>elemento.nombre == producto)){
             isProduct.cantidad += cantidad
+            isProduct.stock -= cantidad
         }else{
             isProduct.cantidad += cantidad
+            isProduct.stock -= cantidad
             carrito.push(isProduct);
         }
         totalCarrito = listaProductos.reduce ((acumulador, isProduct)=>{
@@ -68,4 +179,4 @@ function realizarCompra() {
     }
 }
 
-realizarCompra();
+realizarCompra();*/
