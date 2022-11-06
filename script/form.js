ec9afcaf686792f44af0b7c74bd419c3638b5e34
Nadia Santiago <nadusantiago@gmail.com>
2022-11-06 15:56:22 -0300

    validacion formulario

--- script/form.js
+++ script/form.js
@@ -8,15 +8,28 @@ const btnComprar = document.querySelector("#btn-comprar")
 let datosEnvio = []
 let validar = 0
 
-
-document.addEventListener('DOMContentLoaded', ()=>{
-    
-    if(localStorage.getItem('datosEnvio')){
-        datosEnvio = JSON.parse(localStorage.getItem('datosEnvio'));
+btnComprar.addEventListener("click", (e)=>{
+    checkInputs();
+    if(validar==5){
         enviarDatos();
+    }else{
+        e.preventDefault();
+        validar=0;
     }
-})
-btnComprar.addEventListener("click", enviarDatos);
+});
+
+function errorFor(input, mensaje){
+    const formControl = input.parentElement;
+    const small = formControl.querySelector("small");
+    small.innerText = mensaje;
+    formControl.className = "contacto_info error"
+}
+
+function successFor(input){
+    const formControl = input.parentElement;
+    formControl.className = "contacto_info success";
+    validar ++;
+}
 
 function checkInputs(){
     const nombreValue = nombre.value.trim();
@@ -56,33 +69,21 @@ function checkInputs(){
     console.log(validar)
     return validar;
 }
-function enviarDatos(e){
-    checkInputs();
-    if(validar==5){
-        datosEnvio.push(nombre.value, apellido.value, email.value, tel.value, direccion.value)
-        console.log(datosEnvio)
-        localStorage.setItem('datosEnvio', JSON.stringify(datosEnvio));
-    }
-    validar=0
-    e.preventDefault();
-    console.log(datosEnvio)
+function enviarDatos(){
+    datosEnvio.push(nombre.value, apellido.value, email.value, tel.value, direccion.value)
+    localStorage.setItem('datosEnvio', JSON.stringify(datosEnvio));
 }
 
-
-function errorFor(input, mensaje){
-    const formControl = input.parentElement;
-    const small = formControl.querySelector("small");
-    
-    small.innerText = mensaje;
-    formControl.className = "contacto_info error"
-}
-
-function successFor(input){
-    const formControl = input.parentElement;
-    formControl.className = "contacto_info success";
-    validar ++;
-}
-
+// function mostrarDatos(){
+//     datosContainer.innerHTML =
+//     `<p>Nombre: ${nombreValue}</p>
+//     <p>Apellido: ${apellidoValue}</p>
+//     <p>Email: ${emailValue}</p>
+//     <p>Telefono: ${telValue}</p>
+//     <p>Direccion ${direccionValue}</p>
+//     <p>Observaciones: ${observacionesValue}</p>`
+//     formReset();
+// }
 
 
 
